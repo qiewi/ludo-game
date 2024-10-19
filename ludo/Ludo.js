f32497d6ba75d2d2f87d488e88c6b636592bdbb0
qiewi <105619444+qiewi@users.noreply.github.com>
2024-10-19 18:21:52 +0700

    ft: handle piece click

--- ludo/Ludo.js
+++ ludo/Ludo.js
@@ -1,7 +1,169 @@
 import { UI } from './UI.js';
 
 export class Ludo {
+    currentPositions = {
+        P1: [],
+        P2: []
+    }
+
+    _diceValue;
+    get diceValue() {
+        return this._diceValue;
+    }
+    set diceValue(value) {
+        this._diceValue = value;
+
+        UI.setDiceValue(value);
+    }
+
+    _turn;
+    get turn() {
+        return this._turn;
+    }
+    set turn(value) {
+        this._turn = value;
+        UI.setTurn(value);
+    }
+
+    _state;
+    get state() {
+        return this._state;
+    }
+    set state(value) {
+        this._state = value;
+
+        if(value === STATE.DICE_NOT_ROLLED) {
+            UI.enableDice();
+            UI.unhighlightPieces();
+        } else {
+            UI.disableDice();
+        }
+    }
+
     constructor() {
-        console.log("Hello World!");
+        console.log('Hello World! Lets play Ludo!');
+
+        // this.diceValue = 4;
+        // this.turn = 0;
+        // this.state = STATE.DICE_ROLLED;
+        this.listenDiceClick();
+        this.listenResetClick();
+        this.listenPieceClick();
+
+        this.resetGame();
+        // this.setPiecePosition('P1', 0, 0);
+        // this.setPiecePosition('P2', 0, 1);
+        // this.diceValue = 6;
+        // console.log(this.getEligiblePieces('P1'))
+        
+    }
+
+    listenDiceClick() {
+        UI.listenDiceClick(this.onDiceClick.bind(this))
+    }
+
+    onDiceClick() {
+        console.log('dice clicked!');
+        this.diceValue = 1 + Math.floor(Math.random() * 6);
+        this.state = STATE.DICE_ROLLED;
+        
+        this.checkForEligiblePieces();
+    }
+
+    checkForEligiblePieces() {
+        const player = PLAYERS[this.turn];
+        // eligible pieces of given player
+        const eligiblePieces = this.getEligiblePieces(player);
+        if(eligiblePieces.length) {
+            // highlight the pieces
+            UI.highlightPieces(player, eligiblePieces);
+        } else {
+            this.incrementTurn();
+        }
+    }
+
+    incrementTurn() {
+        this.turn = this.turn === 0 ? 1 : 0;
+        this.state = STATE.DICE_NOT_ROLLED;
+    }
+
+    getEligiblePieces(player) {
+        return [0, 1, 2, 3].filter(piece => {
+            const currentPosition = this.currentPositions[player][piece];
+
+            if(currentPosition === HOME_POSITIONS[player]) {
+                return false;
+            }
+
+            if(
+                BASE_POSITIONS[player].includes(currentPosition)
+                && this.diceValue !== 6
+            ){
+                return false;
+            }
+
+            if(
+                HOME_ENTRANCE[player].includes(currentPosition)
+                && this.diceValue > HOME_POSITIONS[player] - currentPosition
+                ) {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    listenResetClick() {
+        UI.listenResetClick(this.resetGame.bind(this))
+    }
+
+    resetGame() {
+        console.log('reset game');
+        this.currentPositions = structuredClone(BASE_POSITIONS);
+
+        PLAYERS.forEach(player => {
+            [0, 1, 2, 3].forEach(piece => {
+                this.setPiecePosition(player, piece, this.currentPositions[player][piece])
+            })
+        });
+
+        this.turn = 0;
+        this.state = STATE.DICE_NOT_ROLLED;
+    }
+
+    listenPieceClick() {
+        UI.listenPieceClick(this.onPieceClick.bind(this));
+    }
+
+    onPieceClick(event) {
+        const target = event.target;
+
+        if(!target.classList.contains('player-piece') || !target.classList.contains('highlight')) {
+            return;
+        }
+        console.log('piece clicked')
+
+        const player = target.getAttribute('player-id');
+        const piece = target.getAttribute('piece');
+        this.handlePieceClick(player, piece);
+    }
+
+    handlePieceClick(player, piece) {
+        console.log(player, piece);
+        const currentPosition = this.currentPositions[player][piece];
+        
+        if(BASE_POSITIONS[player].includes(currentPosition)) {
+            this.setPiecePosition(player, piece, START_POSITIONS[player]);
+            this.state = STATE.DICE_NOT_ROLLED;
+            return;
+        }
+
+        UI.unhighlightPieces();
+        this.movePiece(player, piece, this.diceValue);
+    }
+
+    setPiecePosition(player, piece, newPosition) {
+        this.currentPositions[player][piece] = newPosition;
+        UI.setPiecePosition(player, piece, newPosition)
     }
 }
